beb1581ee4c47454ec5a06ecc1d9d2603f3067c7
GiuliaSardell <giuliasardelli1234@gmail.com>
2022-01-25 18:25:30 +0100

    faker generati - prova stampa a schermo

--- resources/views/trip.blade.php
+++ resources/views/trip.blade.php
@@ -1,6 +1,33 @@
 @extends('layouts.main')
+
 @section('content')
 <div class="container">
   <h1>Lista viaggi</h1>
+
+  {{-- @dump($trip) --}}
+
+  {{-- <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Destination</th>
+        <th scope="col">Duration</th>
+        <th scope="col">Cost</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      @foreach ($trip as $singleTrip)
+        <tr>
+          <th scope="row">{{$trip->id}}</th>
+          <td>{{$trip->Destination}}</td>
+          <td>{{$trip->Trip_duration}}</td>
+          <td>{{$trip->Cost}}</td>
+        </tr>
+      @endforeach
+      
+      
+    </tbody>
+  </table> --}}
 </div>
 @endsection
